877f573efb1c1877640d89905c754d76af375af0
Александр Морев <a.morev@kryptonite.ru>
2023-01-09 19:04:28 +0300

    Solve Permutations

---
+++ 5. Permutations/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n;
+  cin >> n;
+
+  if (n == 2 || n == 3) {
+    cout << "NO SOLUTION\n";
+    return 0;
+  }
+
+  for (int i = 2; i <= n; i += 2) {
+    cout << i << " ";
+  }
+  for (int i = 1; i <= n; i += 2) {
+    cout << i << " ";
+  }
+  cout << "\n";
+
+  return 0;
+}
