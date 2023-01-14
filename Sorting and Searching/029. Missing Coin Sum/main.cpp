580ed4cb48fbc497f831eecb7a768965c010c174
Александр Морев <a.morev@kryptonite.ru>
2023-01-14 13:49:43 +0300

    Solve Missing Coin Sum

---
+++ Sorting and Searching/029. Missing Coin Sum/main.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define int long long int
+#define endl "\n"
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define repr(i, a, b) for (int i = a; i >= b; i--)
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL);
+#define mod 1000000007 // 1e9+7
+#define inf 1e18       // 1e18
+#define PI 3.1415926535897932384626
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define ps(x, y) fixed << setprecision(y) << x
+#define mk(arr, n, type) type *arr = new type[n];
+#define w(x)                                                                   \
+  int x;                                                                       \
+  cin >> x;                                                                    \
+  while (x--)
+#define print(x) cout << x << endl;
+#define print2(x, y) cout << x << " " << y << endl;
+#define print3(x, y, z) cout << x << " " << y << " " << z << endl;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+// online judge
+void online_judge() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+  int n;
+  cin >> n;
+
+  vi nums;
+  rep(i, 0, n) {
+    int a;
+    cin >> a;
+    nums.pb(a);
+  }
+
+  sort(all(nums));
+
+  ll sum = 0;
+  rep(i, 0, n) {
+    if (sum + 1 < nums[i]) {
+      break;
+    }
+    sum += nums[i];
+  }
+
+  print(sum + 1);
+}
+
+int main() {
+  fastio();
+  online_judge();
+  solve();
+  return 0;
+}
