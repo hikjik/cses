76190fbaf49d62fe3e62f670fa38dd0278161cff
Морев Александр <hikjik@yandex.ru>
2023-09-27 00:33:04 +0300

    Weird Algorithm

---
+++ solutions/weird-algorithm/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  long long n;
+  std::cin >> n;
+
+  std::cout << n;
+  while (n > 1) {
+    n = n & 1 ? 3 * n + 1 : n >> 1;
+    std::cout << " " << n;
+  }
+  std::cout << "\n";
+
+  return 0;
+}
