c5c02af245ed7410f5186c72975fdd79744174f1
Морев Александр <hikjik@yandex.ru>
2023-09-29 20:30:35 +0300

    Refactor

--- solutions/point-location-test/main.cpp
+++ solutions/point-location-test/main.cpp
@@ -15,6 +15,11 @@ Point operator-(const Point &a, const Point &b) {
   return {a.x - b.x, a.y - b.y};
 }
 
+std::istream &operator>>(std::istream &in, Point &p) {
+  in >> p.x >> p.y;
+  return in;
+}
+
 long long CrossProduct(const Point &a, const Point &b) {
   return 1LL * a.x * b.y - 1LL * a.y * b.x;
 }
@@ -27,7 +32,7 @@ int main() {
 
   while (t--) {
     Point s1, s2, p;
-    std::cin >> s1.x >> s1.y >> s2.x >> s2.y >> p.x >> p.y;
+    std::cin >> s1 >> s2 >> p;
 
     const auto cross_product = CrossProduct(p - s1, p - s2);
     if (cross_product > 0) {
