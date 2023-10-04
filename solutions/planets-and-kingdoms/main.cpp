7fad8a71c1577a56a38ca347b977b076ca5d9fb1
Морев Александр <hikjik@yandex.ru>
2023-10-04 11:44:26 +0300

    Update SCC algo

--- solutions/planets-and-kingdoms/main.cpp
+++ solutions/planets-and-kingdoms/main.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <numeric>
+#include <unordered_map>
 #include <vector>
 
 void fast_io() {
@@ -10,6 +11,41 @@ void fast_io() {
 }
 
 using Graph = std::vector<std::vector<int>>;
+
+template <typename T> class Partition {
+public:
+  template <typename U> friend class PartitionBuilder;
+
+  int GroupsNumber() const { return groups_.size(); }
+
+  int GroupId(const T &element) const { return group_id_.at(element); }
+
+  const std::vector<T> &ElementsFromGroup(int group_id) const {
+    return groups_[group_id];
+  }
+
+private:
+  std::unordered_map<T, int> group_id_;
+  std::vector<std::vector<T>> groups_;
+};
+
+template <typename T> class PartitionBuilder {
+public:
+  int CreateNewGroup() {
+    partition_.groups_.emplace_back();
+    return partition_.GroupsNumber() - 1;
+  }
+
+  void AddElementToGroup(int group_id, const T &element) {
+    partition_.groups_[group_id].push_back(element);
+    partition_.group_id_[element] = group_id;
+  }
+
+  Partition<T> Build() { return partition_; }
+
+private:
+  Partition<T> partition_;
+};
 
 class DepthFirstSearchVisitor {
 public:
@@ -31,18 +67,20 @@ private:
 
 class ComponentBuilder : public DepthFirstSearchVisitor {
 public:
-  explicit ComponentBuilder(std::vector<int> *components, int *component_id)
-      : components_(*components), current_component_id_(*component_id) {}
+  explicit ComponentBuilder(PartitionBuilder<int> *partition_builder)
+      : partition_builder_(*partition_builder) {}
 
-  void StartVertex(int u) override { ++current_component_id_; }
+  void StartVertex(int u) override {
+    current_component_id_ = partition_builder_.CreateNewGroup();
+  }
 
   void DiscoverVertex(int u) override {
-    components_[u] = current_component_id_;
+    partition_builder_.AddElementToGroup(current_component_id_, u);
   }
 
 private:
-  std::vector<int> &components_;
-  int &current_component_id_;
+  PartitionBuilder<int> &partition_builder_;
+  int current_component_id_;
 };
 
 template <typename Visitor>
@@ -99,15 +137,13 @@ std::vector<int> GetVerticesInReversedPostOrder(const Graph &graph) {
   return order;
 }
 
-std::pair<int, std::vector<int>>
-FindStronglyConnectedComponent(const Graph &graph) {
+Partition<int> FindStronglyConnectedComponent(const Graph &graph) {
   const auto vertices_order = GetVerticesInReversedPostOrder(graph);
   const auto transpose_graph = BuildTransposeGraph(graph);
-  std::vector<int> components(graph.size());
-  int components_count = 0;
-  ComponentBuilder component_builder(&components, &components_count);
+  PartitionBuilder<int> builder;
+  ComponentBuilder component_builder(&builder);
   TraverseGraphInDfsOrder(transpose_graph, component_builder, vertices_order);
-  return {components_count, components};
+  return builder.Build();
 }
 
 int main() {
@@ -124,11 +160,11 @@ int main() {
     graph[a - 1].push_back(b - 1);
   }
 
-  const auto [count, components] = FindStronglyConnectedComponent(graph);
+  const auto partition = FindStronglyConnectedComponent(graph);
 
-  std::cout << count << "\n";
-  for (auto a : components) {
-    std::cout << a << " ";
+  std::cout << partition.GroupsNumber() << "\n";
+  for (int u = 0; u < n; ++u) {
+    std::cout << partition.GroupId(u) + 1 << " ";
   }
   std::cout << "\n";
 
