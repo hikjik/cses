854f7a3a85a382b2dfb51cdd1e86fc622a2a0592
Морев Александр <hikjik@yandex.ru>
2023-10-06 16:18:27 +0300

    Update Building Roads

--- solutions/building-roads/main.cpp
+++ solutions/building-roads/main.cpp
@@ -1,52 +1,16 @@
 #include <iostream>
 #include <vector>
 
-class DisjointSets {
-public:
-  explicit DisjointSets(size_t size) : sets_(size), count_(size) {
-    for (size_t i = 0; i < sets_.size(); ++i) {
-      sets_[i].parent = i;
+using Graph = std::vector<std::vector<int>>;
+
+void DepthFirstSearch(int u, const Graph &graph, std::vector<bool> *visited) {
+  (*visited)[u] = true;
+  for (auto v : graph[u]) {
+    if (!(*visited)[v]) {
+      DepthFirstSearch(v, graph, visited);
     }
   }
-
-  size_t Count() { return count_; }
-
-  size_t Parent(size_t i) {
-    if (sets_[i].parent != i) {
-      sets_[i].parent = Parent(sets_[i].parent);
-    }
-    return sets_[i].parent;
-  }
-
-  bool Merge(size_t u, size_t v) {
-    size_t i = Parent(u), j = Parent(v);
-    if (i == j) {
-      return false;
-    }
-    count_--;
-
-    if (sets_[i].rank > sets_[j].rank) {
-      sets_[j].parent = i;
-    } else {
-      sets_[i].parent = j;
-      if (sets_[i].rank == sets_[j].rank) {
-        ++sets_[j].rank;
-      }
-    }
-    return true;
-  }
-
-private:
-  struct DisjointSetsElement {
-    DisjointSetsElement() : parent(0), rank(0) {}
-
-    size_t parent;
-    size_t rank;
-  };
-
-  std::vector<DisjointSetsElement> sets_;
-  size_t count_;
-};
+}
 
 void FastIO() {
   std::ios_base::sync_with_stdio(false);
@@ -59,24 +23,28 @@ int main() {
   int n, m;
   std::cin >> n >> m;
 
-  DisjointSets sets(n + 1);
+  Graph graph(n);
   while (m--) {
     int a, b;
     std::cin >> a >> b;
+    --a, --b;
 
-    sets.Merge(a, b);
+    graph[a].push_back(b);
+    graph[b].push_back(a);
   }
 
-  std::vector<std::pair<int, int>> required_roads;
-  for (int i = 2; i <= n; ++i) {
-    if (sets.Merge(i - 1, i)) {
-      required_roads.push_back({i - 1, i});
+  std::vector<int> roads;
+  std::vector<bool> visited(n);
+  for (int u = 0; u < n; ++u) {
+    if (!visited[u]) {
+      roads.push_back(u);
+      DepthFirstSearch(u, graph, &visited);
     }
   }
 
-  std::cout << required_roads.size() << "\n";
-  for (const auto &[a, b] : required_roads) {
-    std::cout << a << " " << b << "\n";
+  std::cout << roads.size() - 1 << "\n";
+  for (size_t i = 0; i + 1 < roads.size(); ++i) {
+    std::cout << roads[i] + 1 << " " << roads[i + 1] + 1 << "\n";
   }
 
   return 0;
