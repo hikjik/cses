0c82ebca8732e15c7daa6cf1a00b3043225b750f
Морев Александр <hikjik@yandex.ru>
2023-11-11 21:58:00 +0300

    Fix Sum of Four Values

--- solutions/sum-of-four-values/main.cpp
+++ solutions/sum-of-four-values/main.cpp
@@ -19,36 +19,30 @@ int main() {
     return 0;
   }
 
-  std::vector<int> arr(n);
-  for (auto &a : arr) {
+  std::vector<std::pair<long long, int>> arr(n);
+  for (int i = 0; i < n; ++i) {
+    int a;
     std::cin >> a;
+    arr[i] = {a, i + 1};
   }
 
-  std::vector<std::tuple<long long, int, int>> pairs_sum;
-  pairs_sum.reserve(n * (n - 1) / 2);
+  std::sort(arr.begin(), arr.end());
   for (int i = 0; i < n; ++i) {
     for (int j = i + 1; j < n; ++j) {
-      pairs_sum.push_back({arr[i] + arr[j], i + 1, j + 1});
+      int l = j + 1, r = n - 1;
+      while (l < r) {
+        const auto sum =
+            arr[i].first + arr[j].first + arr[l].first + arr[r].first;
+        if (sum == x) {
+          std::cout << arr[i].second << ' ' << arr[j].second << ' '
+                    << arr[l].second << ' ' << arr[r].second << '\n';
+          return 0;
+        }
+        sum < x ? ++l : --r;
+      }
     }
-  }
-
-  std::sort(pairs_sum.begin(), pairs_sum.end());
-
-  size_t l = 0, r = pairs_sum.size() - 1;
-  while (l < r) {
-    const auto &[a, i, j] = pairs_sum[l];
-    const auto &[b, s, t] = pairs_sum[r];
-    const auto sum = a + b;
-
-    if (sum == x && std::set<int>({i, j, s, t}).size() == 4) {
-      std::cout << i << " " << j << " " << s << " " << t << "\n";
-      return 0;
-    }
-
-    sum < x ? ++l : --r;
   }
 
   std::cout << "IMPOSSIBLE\n";
-
   return 0;
 }
